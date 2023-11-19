2a07898fa1227473495bb25c60414702088571c1
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-11-19 17:45:25 +0100

    Regen example

--- examples/print_data_marc_leader.pl
+++ examples/print_data_marc_leader.pl
@@ -33,4 +33,19 @@ my $data_marc_leader = Data::MARC::Leader->new(
 print scalar $print->print($data_marc_leader), "\n";
 
 # Output:
-# TODO
+# Record length: 2200
+# Record status: Corrected or revised
+# Type of record: Cartographic material
+# Bibliographic level: Monograph/Item
+# Type of control: No specified type
+# Character coding scheme: UCS/Unicode
+# Indicator count: Number of character positions used for indicators
+# Subfield code count: Number of character positions used for a subfield code (2)
+# Base address of data: 541
+# Encoding level: Full level
+# Descriptive cataloging form: ISBD punctuation included
+# Multipart resource record level: Not specified or not applicable
+# Length of the length-of-field portion: Number of characters in the length-of-field portion of a Directory entry (4)
+# Length of the starting-character-position portion: Number of characters in the starting-character-position portion of a Directory entry (5)
+# Length of the implementation-defined portion: Number of characters in the implementation-defined portion of a Directory entry (0)
+# Undefined: Undefined
