5c09fc9abee1a400be2336c4fc6b194c48ca7c5b
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2026-04-15 16:33:59 +0200

    Prepare for 0.07 version

--- Print.pm
+++ Print.pm
@@ -9,7 +9,7 @@ use Error::Pure qw(err);
 use MARC::Leader::L10N 0.03;
 use Mo::utils::Language 0.05 qw(check_language_639_1);
 
-our $VERSION = 0.06;
+our $VERSION = 0.07;
 
 # Constructor.
 sub new {
@@ -377,6 +377,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.06
+0.07
 
 =cut
